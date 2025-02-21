fd5a7e77c77730d131178211cfbbcc0d981888b3
Willian Carlos Gomes <gomes.cwillian@gmail.com>
2025-02-21 13:41:24 -0300

    chore: Implementing validators

--- src/http_types/http_request.py
+++ src/http_types/http_request.py
@@ -1,4 +1,4 @@
 class HttpRequest:
-    def __init__(self, body: dict = None, param: dict = Nome) -> None:
+    def __init__(self, body: dict = None, param: dict = None) -> None:
         self.body = body
         self.param = param
